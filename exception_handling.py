40e140c9893b146f566445a29f2110989fc48625
unknown <samuelmonish1@gmail.com>
2021-03-04 17:53:52 +0530

    exception handling method to solve

--- exception_handling.py
+++ exception_handling.py
@@ -1,6 +1,10 @@
 a = int(input('Enter the number 1: '))
 b = int(input('Enter the number 2: '))
 
-c = a / b
-
+try:
+    c = a / b
+except Exception as e:
+    print('Exception error: ', e)
+    c = None
+    
 print(f'The division of {a} by {b} is: ', c)
